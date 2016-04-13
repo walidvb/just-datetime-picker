ec9eebe4db834e2e2fc6e4afb575560c96b40a1e
Walid <walidvb@gmail.com>
2016-04-13 22:18:33 +0200

    adds different id to hour and minute fields

--- lib/just-datetime-picker/formtastic-input.rb
+++ lib/just-datetime-picker/formtastic-input.rb
@@ -1,12 +1,12 @@
 module Formtastic
   module Inputs
-    class JustDatetimePickerInput 
+    class JustDatetimePickerInput
       include ::Formtastic::Inputs::Base
 
       def to_html
         input_wrapping do
           combined_value = builder.object.send(method)
-          
+
           hour_value_raw = builder.object.send("#{method}_time_hour")
           if not hour_value_raw.nil?
             hour_value = hour_value_raw
@@ -27,17 +27,15 @@ module Formtastic
 
           hour_value   = sprintf("%02d", hour_value)
           minute_value = sprintf("%02d", minute_value)
-          
+
           label_html <<
           builder.text_field("#{method}_date", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-date datepicker", :value => builder.object.send("#{method}_date"), :maxlength => 10, :size => 10 })) <<
 
-          builder.number_field("#{method}_time_hour", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-hour", :value => hour_value, :maxlength => 2, :size => 2, min: 0, max: 23 })) <<
+          builder.number_field("#{method}_time_hour", input_html_options.merge({ :id => "#{method}_time_hour", :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-hour", :value => hour_value, :maxlength => 2, :size => 2, min: 0, max: 23 })) <<
           ":" <<
-          builder.number_field("#{method}_time_minute", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-minute", :value => minute_value, :maxlength => 2, :size => 2, min: 0, max: 59 }))
+          builder.number_field("#{method}_time_minute", input_html_options.merge({ :id => "#{method}_time_hour", :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-minute", :value => minute_value, :maxlength => 2, :size => 2, min: 0, max: 59 }))
         end
       end
     end
   end
 end
-
-
