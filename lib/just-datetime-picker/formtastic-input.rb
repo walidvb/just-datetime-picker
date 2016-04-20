1bcf92ad0fdd557a77a9d67d0b3e67bee724e11c
Walid <walidvb@gmail.com>
2016-04-20 11:41:20 +0200

    fix model name for clas

--- lib/just-datetime-picker/formtastic-input.rb
+++ lib/just-datetime-picker/formtastic-input.rb
@@ -27,13 +27,12 @@ module Formtastic
 
           hour_value   = sprintf("%02d", hour_value)
           minute_value = sprintf("%02d", minute_value)
-
           label_html <<
-          builder.text_field("#{method}_date", input_html_options.merge({ :id => "#{builder.object.class.class_name.underscore}_#{method}", :value => builder.object.send("#{method}_date"), :maxlength => 10, :size => 10 })) <<
+          builder.text_field("#{method}_date", input_html_options.merge({ :id => "#{builder.object.class.model_name.param_key}_#{method}_time_hour", :class => "just-datetime-picker-field just-datetime-picker-date datepicker", :value => builder.object.send("#{method}_date"), :maxlength => 10, :size => 10 })) <<
 
-          builder.number_field("#{method}_time_hour", input_html_options.merge({ :id => "#{builder.object.class.class_name.underscore}_#{method}_time_hour", :value => hour_value, :maxlength => 2, :size => 2, min: 0, max: 23 })) <<
+          builder.number_field("#{method}_time_hour", input_html_options.merge({ :id => "#{builder.object.class.model_name.param_key}_#{method}_time_hour", :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-hour", :value => hour_value, :maxlength => 2, :size => 2, min: 0, max: 23 })) <<
           ":" <<
-          builder.number_field("#{method}_time_minute", input_html_options.merge({ :id => "#{builder.object.class.class_name.underscore}_#{method}_time_minute", :value => minute_value, :maxlength => 2, :size => 2, min: 0, max: 59 }))
+          builder.number_field("#{method}_time_minute", input_html_options.merge({ :id => "#{builder.object.class.model_name.param_key}_#{method}_time_minute", :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-minute", :value => minute_value, :maxlength => 2, :size => 2, min: 0, max: 59 }))
         end
       end
     end
